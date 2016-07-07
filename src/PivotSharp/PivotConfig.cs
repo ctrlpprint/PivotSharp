63c1b521df954e6a07c7e567fff239d412ec6a03
David <ctrlpprintblog@gmail.com>
2016-07-08 01:24:37 +1200

    Add some really ugly deserialization code

--- src/PivotSharp/PivotConfig.cs
+++ src/PivotSharp/PivotConfig.cs
@@ -1,20 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web.UI;
+using Newtonsoft.Json;
+using PivotSharp.Aggregators;
+using PivotSharp.Filters;
+using RazorEngine.Compilation.ImpromptuInterface.InvokeExt;
 
 namespace PivotSharp
 {
+	[JsonObject(MemberSerialization.OptIn)]
 	public class PivotConfig
 	{
+		[JsonProperty]
 		public IList<string> Rows { get; set; }
+		[JsonProperty]
 		public IList<string> Cols { get; set; }
 		public Func<IAggregator> Aggregator { get; set; }
+
+		[JsonProperty]
+		protected string[] AggregatorName {
+			get {
+				if (Aggregator == null) return new string[]{};
+				var aggregator = Aggregator();
+				return new []{aggregator.SqlFunctionName, aggregator.ColumnName};
+			}
+			set {
+				if(value.Length < 2) throw new ArgumentException("Require at least one element");
+				Aggregator = FromString(value[0], value.Length > 1 ? value[1] : null);
+			} 
+		}
+
+
+		[JsonProperty()]
+		protected dynamic[] FilterNames {
+			get {
+				var ret = new dynamic[Filters.Count];
+				foreach (var filter in Filters) {
+					ret[Filters.IndexOf(filter)].Name = filter.GetType().Name;
+					ret[Filters.IndexOf(filter)].Value = filter.ParameterValue;
+					ret[Filters.IndexOf(filter)].FieldName = filter.FieldName;
+
+				}
+				return ret;
+			}
+			set {
+				Filters = new List<IFilter>();
+				foreach (var details in value) {
+					((List<IFilter>)Filters).Add(FromString(details)); // Cast to List to fix a dynamic binding issue: http://stackoverflow.com/questions/15920844/system-collections-generic-ilistobject-does-not-contain-a-definition-for-ad
+				}
+			}
+		}
+
+
+		private IFilter FromString(dynamic details) {
+
+			var type = details.ParameterValue.Value.GetType();
+
+			if (details.Name == "Equals") {
+				var constructedClass = typeof (Equals<>).MakeGenericType(type);
+				return (IFilter)Activator.CreateInstance(constructedClass, new object[]{ details.FieldName.Value, details.ParameterValue.Value});
+				
+			}
+			if (details.Name == "GreaterThan") {
+				var constructedClass = typeof(GreaterThan<>).MakeGenericType(type);
+				return (IFilter)Activator.CreateInstance(constructedClass, new object[] { details.FieldName.Value, details.ParameterValue.Value });
+
+			}
+			throw new NotImplementedException();
+		}
+
+		private Func<IAggregator> FromString(string name, string columnName) {
+
+			switch (name) {
+				case "Count":
+					return () => new Count();
+				case "Sum":
+					return () => new Sum(columnName);
+			}
+			throw new NotImplementedException();
+		}
+
+		[JsonProperty]
 		public bool FillTable { get; set; }
+
 		public IList<IFilter> Filters { get; set; }
+
+
 
 		public PivotConfig() {
 			Rows = new List<string>();
 			Cols = new List<string>();
 			Filters = new List<IFilter>();
 		}
+
+
 	}
 }
---
+++ tests/PivotSharp.Tests/ConfigSerializationTests.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using PivotSharp.Aggregators;
+using PivotSharp.Filters;
+
+namespace PivotSharp.Tests
+{
+	[TestFixture]
+	public class ConfigSerializationTests
+	{
+		[Test]
+		public void Can_Deserialize() {
+
+			var config = new PivotConfig() {
+				Rows = new[] {"Region", "Country"},
+				Cols = new[] {"Source"},
+				Aggregator = () => new Sum("Revenue"),
+				Filters = new IFilter[] {
+					new GreaterThan<int>("Year", 2015),
+					new Equals<string>("Category", "Web"),
+				},
+				FillTable = true
+			};
+
+			// var json = JsonConvert.SerializeObject(config);
+			// Debug.WriteLine(json);
+
+			var json = @"
+				{
+					'Rows':['Region','Country'],
+					'Cols':['Category'],
+					'AggregatorName':['Sum','Revenue'],
+					'FillTable':true,
+					'FilterNames':[
+						{ Name: 'GreaterThan', FieldName: 'Year', ParameterValue: 2015 },
+						{ Name: 'Equals', FieldName: 'Category', ParameterValue: 'Web' }
+					]
+				}
+";
+
+
+			var deserialized = JsonConvert.DeserializeObject<PivotConfig>(json);
+
+			Assert.NotNull(deserialized);
+			Assert.AreEqual(2, deserialized.Rows.Count);
+			Assert.AreEqual("Region", deserialized.Rows[0]);
+			Assert.AreEqual("Country", deserialized.Rows[1]);
+			Assert.AreEqual(1, deserialized.Cols.Count);
+			Assert.AreEqual("Category", deserialized.Cols[0]);
+			Assert.AreEqual(true, deserialized.FillTable);
+			Assert.AreEqual("Sum", deserialized.Aggregator().SqlFunctionName);
+			Assert.AreEqual("Revenue", deserialized.Aggregator().ColumnName);
+			Assert.AreEqual(0, deserialized.Aggregator().Value);
+			Assert.AreEqual(2, deserialized.Filters.Count);
+			Assert.AreEqual(typeof(GreaterThan<long>), deserialized.Filters[0].GetType());
+			Assert.AreEqual("Year", deserialized.Filters[0].FieldName);
+			Assert.AreEqual(2015, deserialized.Filters[0].ParameterValue);
+			Assert.AreEqual(typeof(Equals<string>), deserialized.Filters[1].GetType());
+			Assert.AreEqual("Category", deserialized.Filters[1].FieldName);
+			Assert.AreEqual("Web", deserialized.Filters[1].ParameterValue);
+
+
+			//Assert.NotNull();
+		}
+		 
+	}
+}
