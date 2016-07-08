25716dfb872d0b575dc65ff39f6fca7424f25679
David <ctrlpprintblog@gmail.com>
2016-07-08 20:36:55 +1200

    Add some stubs for remaining standard aggregators

---
+++ src/PivotSharp/Aggregators/Ave.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+
+namespace PivotSharp.Aggregators
+{
+	public class Ave : IAggregator
+	{
+		public string ColumnName { get; set; }
+		public decimal SumTotal { get; private set; }
+		public int Count { get; private set; }
+
+		public string SqlFunctionName { get { return "Avg"; } }
+
+
+		public Ave(string columnName) {
+			ColumnName = columnName;
+		}
+
+		public void Push(IDataReader record) {
+			var val = record[ColumnName].ToString().ToDecimal();
+			SumTotal += val;
+			Count++;
+			HasEntries = true;
+		}
+
+		public decimal Value { get { return Count > 0 ? Decimal.Divide(SumTotal, (decimal)Count) : 0; } }
+
+		public string FormattedValue { get { return Value.ToString("N2"); } }
+		public bool HasEntries { get; private set; }
+	}
+}
---
+++ src/PivotSharp/Aggregators/Max.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+
+namespace PivotSharp.Aggregators
+{
+	public class Max : IAggregator
+	{
+		public string ColumnName { get; set; }
+		public decimal? Maximum { get; private set; }
+		public string SqlFunctionName { get { return "Max"; } }
+
+		public Max(string columnName) {
+			ColumnName = columnName;
+		}
+
+		public void Push(IDataReader record) {
+			var val = record[ColumnName].ToString().ToDecimal();
+			Maximum = Maximum == null ? val : Math.Max(Maximum.Value, val);
+			HasEntries = true;
+		}
+
+		public decimal Value { get { return Maximum.HasValue ? Maximum.Value : 0; } }
+
+		public string FormattedValue { get { return Value.ToString("N2"); } }
+		public bool HasEntries { get; private set; }
+ 
+	}
+}
