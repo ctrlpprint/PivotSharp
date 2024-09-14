9f964ca40d599cf5b4f2f7abed946fcfc2e36340
David <j.david.wong@gmail.com>
2024-09-14 13:52:22 +1200

    Create a tag helper

--- samples/PivotSharp.WebCore/Program.cs
+++ samples/PivotSharp.WebCore/Program.cs
@@ -13,7 +13,8 @@ public class Program
 		// Configure the HTTP request pipeline.
 		if (!app.Environment.IsDevelopment()) {
 			app.UseExceptionHandler("/Error");
-			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+			// The default HSTS value is 30 days. You may want to change this for production scenarios,
+			// see https://aka.ms/aspnetcore-hsts.
 			app.UseHsts();
 		}
 
---
+++ src/PivotSharp/TagHelpers/PivotTableTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using PivotSharp.Writers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace PivotSharp.TagHelpers;
+
+public class PivotTableTagHelper : TagHelper
+{
+    public PivotTable? Model { get; set; }
+
+    public IHtmlTableMap? Map { get; set; }
+
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = null; // We'll build our own
+
+        if (Model == null)
+        {
+            output.SuppressOutput();
+        }
+        else
+        {
+            output.Content.SetHtmlContent(Model.BuildHtmlTable(map: Map));
+        }
+
+    }
+}
